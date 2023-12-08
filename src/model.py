781778e60f85771990b53d20d05642c9b34aeca2
lizzzi111 <zinovyee@gmail.com>
2023-12-08 22:41:43 +0100

    🎨 cleaunp

--- src/model.py
+++ src/model.py
@@ -1,11 +1,12 @@
 import gc
 
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 import torch
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
-def load_model_tok(model_name: str, freeze:bool=True) -> list:
 
-    if model_name=='CodeT5':
+def load_model_tok(model_name: str, freeze:bool=True) -> list:  # noqa: FBT001, FBT002
+
+    if model_name=="CodeT5":
         model_name="Salesforce/codet5-base-multi-sum"
 
     model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
@@ -14,7 +15,7 @@ def load_model_tok(model_name: str, freeze:bool=True) -> list:
     if freeze:
         for parameter in model.parameters():
             parameter.requires_grad = False
-        for i, m in enumerate(model.decoder.block):        
+        for i, m in enumerate(model.decoder.block):
             #Only un-freeze the last n transformer blocks in the decoder
             if i+1 > 12 - 4:
                 for parameter in m.parameters():
@@ -23,6 +24,6 @@ def load_model_tok(model_name: str, freeze:bool=True) -> list:
     return model, tokenizer
 
 
-def torch_clean():
+def torch_clean():  # noqa: ANN201
     gc.collect()
     torch.cuda.empty_cache()
