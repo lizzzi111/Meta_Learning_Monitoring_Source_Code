c99b8bcd0db092a4402dc1ad8e231934f3dd5663
lizzzi111 <zinovyee@gmail.com>
2024-03-27 10:11:42 +0100

    🎨 add data

--- src/processing_utils.py
+++ src/processing_utils.py
@@ -54,25 +54,25 @@ def prepare_hg_ds(dataset, tokenizer, max_input_length, max_output_length, batch
 
 def compute_metric_with_params(tokenizer, metrics_list=['rouge', 'bleu']):
     def compute_metrics(eval_preds):
-    
+
         preds, labels = eval_preds
-    
+
         if isinstance(preds, tuple):
             preds = preds[0]
-    
+
         decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
-    
+
         # Replace -100 in the labels as we can't decode them.
         labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
         decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
-    
+
         # POST PROCESSING
         decoded_preds, decoded_labels = postprocess_text(decoded_preds, decoded_labels)
-    
+
         results_dict = {}
         for m in metrics_list:
             metric = evaluate.load(m)
-    
+
             if m=='bleu':
                 result = metric.compute(
                     predictions=decoded_preds, references=decoded_labels
@@ -82,7 +82,7 @@ def compute_metric_with_params(tokenizer, metrics_list=['rouge', 'bleu']):
                     predictions=decoded_preds, references=decoded_labels, use_stemmer=True
                 )
             result = {key: value for key, value in result.items() if key!='precisions'}
-    
+
             prediction_lens = [
                 np.count_nonzero(pred != tokenizer.pad_token_id) for pred in preds
             ]
